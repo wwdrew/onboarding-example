767710eafa621a6fca7842e165a7f9934749a422
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2021-12-06 17:25:16 +0000

    feat: create authenticated stack and related screen

---
+++ src/navigation/authenticated.stack.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/authenticated/Home.screen';
+
+export type AuthenticatedStackParamList = {
+  Home: undefined;
+};
+
+const Authenticated = createNativeStackNavigator<AuthenticatedStackParamList>();
+
+const AuthenticatedStack = () => (
+  <Authenticated.Navigator>
+    <Authenticated.Screen name="Home" component={HomeScreen} />
+  </Authenticated.Navigator>
+);
+
+export default AuthenticatedStack;
---
+++ src/screens/authenticated/Home.screen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Center, Heading, Text } from 'native-base';
+
+const HomeScreen = () => (
+  <Center flex={1} px={4}>
+    <StatusBar style="auto" />
+    <Heading>Drew's Awesome App!</Heading>
+    <Text textAlign="center">Buy low! Sell high!</Text>
+  </Center>
+);
+
+export default HomeScreen;
