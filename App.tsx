aa118bc972eb5fc4a24df79236d8e2a7dab0e00a
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2021-12-06 17:26:49 +0000

    feat: add authenticated stack to app and add flag to determine which stack to show

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
+
+import AuthenticatedStack from './src/navigation/authenticated.stack';
 import UnauthenticatedStack from './src/navigation/unauthenticated.stack';
 
 export default function App() {
+  const authenticated = false;
+
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        <UnauthenticatedStack />
+        {authenticated ? <AuthenticatedStack /> : <UnauthenticatedStack />}
       </NavigationContainer>
     </NativeBaseProvider>
   );
