b70f1bfcad0e7160919fd339921e51efad43d0c5
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2021-12-06 11:46:58 +0000

    feat: add native-base for simplified screen styling

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { NativeBaseProvider } from 'native-base';
 import UnauthenticatedStack from './src/navigation/unauthenticated.stack';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <UnauthenticatedStack />
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <UnauthenticatedStack />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
